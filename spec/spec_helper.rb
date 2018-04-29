754923b15b4cc1142b9b9411ac10add209403da2
Sunny Ripert <sunny@sunfox.org>
2018-04-29 17:55:24 +0200

    RSpec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,8 +7,4 @@ RSpec.configure do |config|
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
-
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
-  end
 end
