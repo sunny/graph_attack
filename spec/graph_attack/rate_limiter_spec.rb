e71a7f895ffff6bec4b25c0d8678e88720445bac
Sunny Ripert <sunny@sunfox.org>
2020-08-16 21:45:28 +0200

    Nest specs using GraphQL::Ruby's DSL

--- spec/graph_attack/rate_limiter_spec.rb
+++ spec/graph_attack/rate_limiter_spec.rb
@@ -49,123 +49,125 @@ RSpec.describe GraphAttack::RateLimiter do
     redis.scan_each(match: 'ratelimit:*') { |key| redis.del(key) }
   end
 
-  describe 'fields without rate limiting' do
-    it 'returns data' do
-      result = schema.execute('{ inexpensiveField }', context: context)
-
-      expect(result).not_to have_key('errors')
-      expect(result['data']).to eq('inexpensiveField' => 'result')
-    end
-
-    it 'does not insert rate limits in redis' do
-      schema.execute('{ inexpensiveField }', context: context)
-
-      expect(redis.scan_each(match: 'ratelimit:*').count).to eq(0)
-    end
-  end
-
-  describe 'fields with rate limiting' do
-    it 'inserts rate limits in redis' do
-      schema.execute('{ expensiveField }', context: context)
-
-      key = 'ratelimit:99.99.99.99:graphql-query-expensiveField'
-      expect(redis.scan_each(match: key).count).to eq(1)
-    end
-
-    it 'returns data until rate limit is exceeded' do
-      4.times do
-        result = schema.execute('{ expensiveField }', context: context)
+  context 'when using the GraphQL::Ruby DSL' do
+    describe 'fields without rate limiting' do
+      it 'returns data' do
+        result = schema.execute('{ inexpensiveField }', context: context)
 
         expect(result).not_to have_key('errors')
-        expect(result['data']).to eq('expensiveField' => 'result')
+        expect(result['data']).to eq('inexpensiveField' => 'result')
+      end
+
+      it 'does not insert rate limits in redis' do
+        schema.execute('{ inexpensiveField }', context: context)
+
+        expect(redis.scan_each(match: 'ratelimit:*').count).to eq(0)
       end
     end
 
-    context 'when rate limit is exceeded' do
-      before do
+    describe 'fields with rate limiting' do
+      it 'inserts rate limits in redis' do
+        schema.execute('{ expensiveField }', context: context)
+
+        key = 'ratelimit:99.99.99.99:graphql-query-expensiveField'
+        expect(redis.scan_each(match: key).count).to eq(1)
+      end
+
+      it 'returns data until rate limit is exceeded' do
         4.times do
-          schema.execute('{ expensiveField }', context: context)
-        end
-      end
-
-      it 'returns an error' do
-        result = schema.execute('{ expensiveField }', context: context)
-
-        expected_message = 'Query rate limit exceeded on expensiveField'
-        expect(result['errors']).to eq([{ 'message' => expected_message }])
-        expect(result).not_to have_key('data')
-      end
-
-      context 'when on a different IP' do
-        let(:context2) { { ip: '203.0.113.43' } }
-
-        it 'does not return an error' do
-          result = schema.execute('{ expensiveField }', context: context2)
+          result = schema.execute('{ expensiveField }', context: context)
 
           expect(result).not_to have_key('errors')
           expect(result['data']).to eq('expensiveField' => 'result')
         end
       end
-    end
-  end
 
-  describe 'several fields with rate limiting' do
-    context 'when one rate limit is exceeded' do
-      before do
-        5.times do
-          schema.execute(
+      context 'when rate limit is exceeded' do
+        before do
+          4.times do
+            schema.execute('{ expensiveField }', context: context)
+          end
+        end
+
+        it 'returns an error' do
+          result = schema.execute('{ expensiveField }', context: context)
+
+          expected_message = 'Query rate limit exceeded on expensiveField'
+          expect(result['errors']).to eq([{ 'message' => expected_message }])
+          expect(result).not_to have_key('data')
+        end
+
+        context 'when on a different IP' do
+          let(:context2) { { ip: '203.0.113.43' } }
+
+          it 'does not return an error' do
+            result = schema.execute('{ expensiveField }', context: context2)
+
+            expect(result).not_to have_key('errors')
+            expect(result['data']).to eq('expensiveField' => 'result')
+          end
+        end
+      end
+    end
+
+    describe 'several fields with rate limiting' do
+      context 'when one rate limit is exceeded' do
+        before do
+          5.times do
+            schema.execute(
+              '{ expensiveField expensiveField2 }',
+              context: context,
+            )
+          end
+        end
+
+        it 'returns an error message with only the first field' do
+          result = schema.execute(
             '{ expensiveField expensiveField2 }',
             context: context,
           )
+
+          expected_message = 'Query rate limit exceeded on expensiveField'
+          expect(result['errors']).to eq([{ 'message' => expected_message }])
+          expect(result).not_to have_key('data')
         end
       end
 
-      it 'returns an error message with only the first field' do
-        result = schema.execute(
-          '{ expensiveField expensiveField2 }',
-          context: context,
-        )
+      context 'when both rate limits are exceeded' do
+        before do
+          10.times do
+            schema.execute(
+              '{ expensiveField expensiveField2 }',
+              context: context,
+            )
+          end
+        end
 
-        expected_message = 'Query rate limit exceeded on expensiveField'
-        expect(result['errors']).to eq([{ 'message' => expected_message }])
-        expect(result).not_to have_key('data')
-      end
-    end
-
-    context 'when both rate limits are exceeded' do
-      before do
-        10.times do
-          schema.execute(
+        it 'returns an error message with both fields' do
+          result = schema.execute(
             '{ expensiveField expensiveField2 }',
             context: context,
           )
+
+          expected_message =
+            'Query rate limit exceeded on expensiveField, expensiveField2'
+          expect(result['errors']).to eq([{ 'message' => expected_message }])
+          expect(result).not_to have_key('data')
         end
       end
-
-      it 'returns an error message with both fields' do
-        result = schema.execute(
-          '{ expensiveField expensiveField2 }',
-          context: context,
-        )
-
-        expected_message =
-          'Query rate limit exceeded on expensiveField, expensiveField2'
-        expect(result['errors']).to eq([{ 'message' => expected_message }])
-        expect(result).not_to have_key('data')
-      end
     end
-  end
 
-  context 'with custom redis client' do
-    let(:schema) { Dummy::SchemaWithCustomRedisClient }
-    let(:redis) { Dummy::CUSTOM_REDIS_CLIENT }
+    context 'with custom redis client' do
+      let(:schema) { Dummy::SchemaWithCustomRedisClient }
+      let(:redis) { Dummy::CUSTOM_REDIS_CLIENT }
 
-    describe 'fields with rate limiting' do
-      it 'inserts rate limits in the custom redis client' do
-        schema.execute('{ expensiveField }', context: context)
+      describe 'fields with rate limiting' do
+        it 'inserts rate limits in the custom redis client' do
+          schema.execute('{ expensiveField }', context: context)
 
-        key = 'ratelimit:99.99.99.99:graphql-query-expensiveField'
-        expect(redis.scan_each(match: key).count).to eq(1)
+          key = 'ratelimit:99.99.99.99:graphql-query-expensiveField'
+          expect(redis.scan_each(match: key).count).to eq(1)
+        end
       end
     end
   end
